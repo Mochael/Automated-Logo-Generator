bae92b5ac9d22dd11270cf8d2b94310b07f8a51e
evannwu4 <evannwu@gmail.com>
2018-09-22 00:38:23 -0400

    tracing

--- Illustratorscript.js
+++ Illustratorscript.js
@@ -5,10 +5,14 @@ var image = require("image.png");
 
 //SECTION 1: Export image into Illustrator, center it
 
-aw
+
 
 //SECTION 2: Live trace the image in black and white, with ignore white set to true
-hello world
+var traceItem = image.trace()
+traceItem.tracing.TracingOptions.tracingMode = TracingModeType.TRACINGMODEBLACKANDWHITE;
+traceItem.tracing.TracingOptions.ignoreWhite = true;
+traceItem.tracing.TracingOptions. = true;
+
 //SECTION 3: Analyze the width and height of the image to correctly
 // format it with the text.
 
