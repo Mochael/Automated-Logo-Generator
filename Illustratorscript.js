30124b823ae3f9bfa3d0a5812de12e7a39b9ae92
Evann Wu <evannw@bh140d-a2.andrew.cmu.edu>
2018-09-22 19:56:32 -0400

    boomshakalaka

--- Illustratorscript.js
+++ Illustratorscript.js
@@ -36,6 +36,7 @@ while (!txtFile.eof) {
 
 
 */
+
 var possibleColors = ["red", "blue", "white", "green", "yellow", "black", "orange", "purple", "cyan", "grey", "indigo", "black", "brown","gold","brown"];
 var vowels = ["a", "e", "i", "o", "u"];
 var fancyFonts = ["Athelas","Birch Std","Charlemagne Std Bold","Cooper Std","Trajan Pro", "Apple Chancery"];
@@ -52,35 +53,35 @@ var friendlyHex = ["#00c29e","#00c1ff","#9d99ff","#ffb653","#db4114","ff9baa"];
 
 
 
-// let outputTags = [{ name: 'wall', confidence: 0.9785731434822083 },
+// var outputTags = [{ name: 'wall', confidence: 0.9785731434822083 },
 //   { name: 'indoor', confidence: 0.9387333989143372 },
 //   { name: 'green', confidence: 0.7196652293205261 },
 //   { name: 'vessel', confidence: 0.5777671337127686 },
 //   { name: 'bottle', confidence: 0.570465624332428 } ];
 
-let outputTags = ["wall","indoor","green","vessel","bottle"];
+var outputTags = ["wall","indoor","green","vessel","bottle"];
 
 
 function vowelName(outputTags){
-  let segments = [];
-  for(let i=0; i<outputTags.length;i++){
-    let tempString = "";
+  var segments = [];
+  for(var i=0; i<outputTags.length;i++){
+    var tempString = "";
     if(outputTags[i]!="wall"&&outputTags[i]!="indoor"&&outputTags[i]!="white"){
-      for(let j=0; j<outputTags[i].length;j++){
+      for(var j=0; j<outputTags[i].length;j++){
         tempString += outputTags[i].charAt(j);
-        if(vowels.indexOf(outputTags[i].charAt(j))>=0){
+        if(vowels[outputTags[i].charAt(j)]>=0){
           segments.push(tempString);
         }
       }
     }
   }
-  let maxOne = segments.length;
-  let maxTwo = outputTags.length;
-  let indexOne = Math.floor(Math.random() * Math.floor(maxOne));
-  let indexTwo = Math.floor(Math.random() * Math.floor(maxTwo));
-  let segment = segments[indexOne];
-  let word = outputTags[indexTwo];
-  if(vowels.indexOf(word.charAt(0))>=0){
+  var maxOne = segments.length;
+  var maxTwo = outputTags.length;
+  var indexOne = Math.floor(Math.random() * Math.floor(maxOne));
+  var indexTwo = Math.floor(Math.random() * Math.floor(maxTwo));
+  var segment = segments[indexOne];
+  var word = outputTags[indexTwo];
+  if(vowels[word.charAt(0)]>=0){
     word = word.slice(1, word.length);
   }
   if(Math.floor(Math.random() * Math.floor(2))==1){
@@ -93,10 +94,10 @@ function vowelName(outputTags){
 
 
 function colorName(outputTags){
-  let colors = [];
-  let nouns = [];
-  for(let i=0;i<outputTags.length;i++){
-    if(possibleColors.indexOf(outputTags[i])>=0){
+  var colors = [];
+  var nouns = [];
+  for(var i=0;i<outputTags.length;i++){
+    if(possibleColors[outputTags[i]]>=0){
       colors.push(outputTags[i]);
     }
     else{
@@ -104,10 +105,10 @@ function colorName(outputTags){
     }
   }
   if(colors.length>0 && nouns.length>0){
-    let maxOne = colors.length;
-    let maxTwo = nouns.length;
-    let indexOne = Math.floor(Math.random() * Math.floor(maxOne));
-    let indexTwo = Math.floor(Math.random() * Math.floor(maxTwo));
+    var maxOne = colors.length;
+    var maxTwo = nouns.length;
+    var indexOne = Math.floor(Math.random() * Math.floor(maxOne));
+    var indexTwo = Math.floor(Math.random() * Math.floor(maxTwo));
 
     if(Math.floor(Math.random() * Math.floor(2))==1){
       return colors[indexOne]+nouns[indexTwo];
@@ -120,17 +121,17 @@ function colorName(outputTags){
 }
 
 function randomJoin(outputTags){
-  let name = "";
-  for(let i=0; i<outputTags.length;i++){
-    let tempString = "";
+  var name = "";
+  for(var i=0; i<outputTags.length;i++){
+    var tempString = "";
     if(outputTags[i]!="wall"&&outputTags[i]!="indoor"&&outputTags[i]!="white"){
-      let max = outputTags[i].length;
-      let indexOne = Math.floor(Math.random() * Math.floor(max));
-      let indexTwo = Math.floor(Math.random() * Math.floor(max));
-      let large = Math.max(indexOne,indexTwo);
-      let small = Math.min(indexOne,indexTwo);
+      var max = outputTags[i].length;
+      var indexOne = Math.floor(Math.random() * Math.floor(max));
+      var indexTwo = Math.floor(Math.random() * Math.floor(max));
+      var large = Math.max(indexOne,indexTwo);
+      var small = Math.min(indexOne,indexTwo);
 
-      for(let j=small; j<large;j++){
+      for(var j=small; j<large;j++){
         tempString += outputTags[i].charAt(j);
       }
     }
@@ -146,10 +147,10 @@ function getBrandName(outputTags){
   //If user inputs certain method or brandname choose that and Otherwise run random methods
 
   //Random int between 0-9
-  let max = 3;
-  let index = Math.floor(Math.random() * Math.floor(max));
+  var max = 3;
+  var index = Math.floor(Math.random() * Math.floor(max));
   //console.log(index);
-  let brandname;
+  var brandname;
   switch(index) {
     case 0:
       brandname = colorName(outputTags);
@@ -167,8 +168,8 @@ function getBrandName(outputTags){
 
 
 function pickStyle(){
-  let style;
-  let index = Math.floor(Math.random() * Math.floor(3));
+  var style;
+  var index = Math.floor(Math.random() * Math.floor(3));
   switch(index) {
     case 0:
       style = "fancy";
@@ -184,46 +185,46 @@ function pickStyle(){
 }
 
 function fontRandmizer(style){
-  let font;
+  var font;
   if(style=="friendly"){
-    let index = Math.floor(Math.random() * Math.floor(friendlyFonts.length));
+    var index = Math.floor(Math.random() * Math.floor(friendlyFonts.length));
     font = friendlyFonts[index];
   }
   else if(style=="fancy"){
-    let index = Math.floor(Math.random() * Math.floor(fancyFonts.length));
+    var index = Math.floor(Math.random() * Math.floor(fancyFonts.length));
     font = fancyFonts[index];
   }
   else if(style=="kidly"){
-    let index = Math.floor(Math.random() * Math.floor(kidlyFonts.length));
+    var index = Math.floor(Math.random() * Math.floor(kidlyFonts.length));
     font = kidlyFonts[index];
   }
   else{
-    let index = Math.floor(Math.random() * Math.floor(defaultFonts.length));
+    var index = Math.floor(Math.random() * Math.floor(defaultFonts.length));
     font = defaultFonts[index];
   }
 return font;
 }
 
 function pickColorFunc(style){
-  let returnArr=[];
+  var returnArr=[];
   if(style=="fancy"){
-    let index = Math.floor(Math.random() * Math.floor(fancyHex.length));
+    var index = Math.floor(Math.random() * Math.floor(fancyHex.length));
     returnArr.push(fancyHex[index]);
   }
   else if(style=="friendly"){
-    let indexOne = Math.floor(Math.random() * Math.floor(friendlyHex.length));
+    var indexOne = Math.floor(Math.random() * Math.floor(friendlyHex.length));
     returnArr.push(friendlyHex[indexOne]);
-    let indexTwo = Math.floor(Math.random() * Math.floor(friendlyHex.length));
+    var indexTwo = Math.floor(Math.random() * Math.floor(friendlyHex.length));
     returnArr.push(friendlyHex[indexTwo]);
   }
   else if(style=="kidly"){
-    let indexOne = Math.floor(Math.random() * Math.floor(kidlyHex.length));
+    var indexOne = Math.floor(Math.random() * Math.floor(kidlyHex.length));
     returnArr.push(kidlyHex[indexOne]);
-    let indexTwo = Math.floor(Math.random() * Math.floor(kidlyHex.length));
+    var indexTwo = Math.floor(Math.random() * Math.floor(kidlyHex.length));
     returnArr.push(kidlyHex[indexTwo]);
-    let indexThree = Math.floor(Math.random() * Math.floor(kidlyHex.length));
+    var indexThree = Math.floor(Math.random() * Math.floor(kidlyHex.length));
     returnArr.push(kidlyHex[indexThree]);
-    let indexFour = Math.floor(Math.random() * Math.floor(kidlyHex.length));
+    var indexFour = Math.floor(Math.random() * Math.floor(kidlyHex.length));
     returnArr.push(kidlyHex[indexFour]);
   }
   return returnArr;
@@ -231,10 +232,10 @@ function pickColorFunc(style){
 
 
 function userFeedback(userLst){
-  let imageNumber = fileName.length-1;
-  let lst = masterLst[imageNumber];
-  let returnLst = [];
-  for(let i=0; i<userLst.length;i++){
+  var imageNumber = fileName.length-1;
+  var lst = masterLst[imageNumber];
+  var returnLst = [];
+  for(var i=0; i<userLst.length;i++){
     if (userLst[i] == False){
       returnLst.push(userLst[i]);
     }
@@ -246,11 +247,12 @@ function userFeedback(userLst){
 }
 
 function mainRun(outputTags, brandName, logoStyle, filePath){
-  let tempBrand;
-  let tempStyle;
-  let tempColors;
-  let tempFont;
-  let tempArr;
+  var brandName = false;
+  var tempBrand;
+  var tempStyle;
+  var tempColors;
+  var tempFont;
+  var tempArr;
   if(brandName==false){
     tempBrand = getBrandName(outputTags);
   }
@@ -276,78 +278,92 @@ var myOutput = mainRun(outputTags, false, false, "ok");
 function createImage(fil, type, color, font, name){
   doc = app.activeDocument;
   var txt = doc.textFrames.add();
-  //image comes in as image.png
-  //var image = new File("image.jpeg");
+//image comes in as image.png
+//var image = new File("image.jpeg");
 
 
-  //SECTION 1: Export image into Illustrator, center it
+//SECTION 1: Export image into Illustrator, center it
 
 
-  var imagenew = doc.placedItems.add();
-  app.coordinateSystem = CoordinateSystem.ARTBOARDCOORDINATESYSTEM;
-  imagenew.file = new File(fil)//"/Users/evannw/Documents/GitHub/Automated-Logo-Generator/image6.JPG");
-  var ratio = doc.height/imagenew.height;
-  imagenew.height = doc.height;
-  imagenew.width = imagenew.width*ratio;
-  var abIdx = doc.artboards.getActiveArtboardIndex();
-  var actAbBds = doc.artboards[abIdx].artboardRect;
-  imagenew.position = new Array ((actAbBds[2]-actAbBds[0])/2 - imagenew.width/2, (actAbBds[3]-actAbBds[1])/2  + imagenew.height/2);
+var imagenew = doc.placedItems.add();
+app.coordinateSystem = CoordinateSystem.ARTBOARDCOORDINATESYSTEM;
+imagenew.file = new File("/Users/evannw/Documents/GitHub/Automated-Logo-Generator/image6.jpg");
+var ratio = doc.height/imagenew.height;
+imagenew.height = doc.height;
+imagenew.width = imagenew.width*ratio;
+var abIdx = doc.artboards.getActiveArtboardIndex();
+var actAbBds = doc.artboards[abIdx].artboardRect;
+imagenew.position = new Array ((actAbBds[2]-actAbBds[0])/2 - imagenew.width/2, (actAbBds[3]-actAbBds[1])/2  + imagenew.height/2);
 
 
-  //SECTION 2: Live trace the image in black and white, with ignore white set to true
-  var traceItem = imagenew.trace();
-  //app.redraw();
-  traceItem.tracing.tracingOptions.tracingMode = TracingModeType.TRACINGMODEBLACKANDWHITE;
-  //traceItem.tracing.tracingOptions.ignoreWhite = true;
-  traceItem.tracing.tracingOptions.pathFidelity = 78;
-  traceItem.tracing.tracingOptions.noiseFidelity = 0;
-  app.redraw();
-  var GroupedItem = traceItem.tracing.expandTracing(true);
-  //GroupedItem.selected = true;
-  //doc.pathItems[doc.pathItems.length-2].selected = true;
-  //doc.pathItems[doc.pathItems.length-3].selected = true;
-  doc.groupItems[0].selected = false;
-  var logoMark = doc.groupItems[0].groupItems[0];
-  var LogoMarkItems = doc.groupItems[0].groupItems[0].pathItems;
-  //doc.groupItems[0].groupItems[0].pathItems[7].selected=true;
+//SECTION 2: Live trace the image in black and white, with ignore white set to true
+var traceItem = imagenew.trace();
+//app.redraw();
+traceItem.tracing.tracingOptions.tracingMode = TracingModeType.TRACINGMODEBLACKANDWHITE;
+//traceItem.tracing.tracingOptions.ignoreWhite = true;
+traceItem.tracing.tracingOptions.pathFidelity = 78;
+traceItem.tracing.tracingOptions.noiseFidelity = 0;
+app.redraw();
+var GroupedItem = traceItem.tracing.expandTracing(true);
+//GroupedItem.selected = true;
+//doc.pathItems[doc.pathItems.length-2].selected = true;
+//doc.pathItems[doc.pathItems.length-3].selected = true;
+doc.groupItems[0].selected = false;
+var logoMark = doc.groupItems[0].groupItems[0];
+var LogoMarkItems = doc.groupItems[0].groupItems[0].pathItems;
+//doc.groupItems[0].groupItems[0].pathItems[7].selected=true;
 
-  for(i = 0; i < LogoMarkItems.length; i++){
-    if(LogoMarkItems[i].pathPoints.length < 20){
-      LogoMarkItems[i].selected = true;
-    }
+for(i = 0; i < LogoMarkItems.length; i++){
+  if(LogoMarkItems[i].pathPoints.length < 20){
+    LogoMarkItems[i].selected = true;
   }
-  app.cut();
-  doc.pathItems[doc.pathItems.length-1].remove();
-  var baseMark = doc.pathItems[doc.pathItems.length-1];
-  baseMark.selected = true;
-  //app.copy();
-  //GroupedItem.remove();
-  //app.paste();
-  //var logoMark = doc.pathItems[0];
-  //doc.pathItems[0].selected = true;
-  //groupGroupedItem.selected = true;
-  //pathItem totalShape = groupGroupedItem.pathItems[0];
-  //totalShape.selected = true;
-  var len = logoMark.position[0];
-  var hei = logoMark.position[1] - logoMark.height;
+}
+app.cut();
+doc.pathItems[doc.pathItems.length-1].remove();
+var baseMark = doc.pathItems[doc.pathItems.length-1];
+baseMark.selected = true;
+//app.copy();
+//GroupedItem.remove();
+//app.paste();
+//var logoMark = doc.pathItems[0];
+//doc.pathItems[0].selected = true;
+//groupGroupedItem.selected = true;
+//pathItem totalShape = groupGroupedItem.pathItems[0];
+//totalShape.selected = true;
 
-  //var logoCenter = new Array(logoMark.position[0]-(11/10)*logoMark.height, logoMark.position[1] + (1/2)*logoMark.length);
-  txt.contents = name;
-  var textArtRange = doc.textFrames[0].textRange;
-  textArtRange.characterAttributes.textFont = app.textFonts[0];
-  for(j = 0; j < app.textFonts.length; j++) {
-    if (app.textFonts[j] == font) {
-      textArtRange.characterAttributes.textFont = app.textFonts[j];
-    }
+logoMark.width = 400/logoMark.height * logoMark.width;
+logoMark.height = 400;
+var len = logoMark.position[0];
+var hei = logoMark.position[1] - logoMark.height;
+
+//var logoCenter = new Array(logoMark.position[0]-(11/10)*logoMark.height, logoMark.position[1] + (1/2)*logoMark.length);
+txt.contents = name;
+var textArtRange = doc.textFrames[0].textRange;
+textArtRange.characterAttributes.textFont = app.textFonts[0];
+for(j = 0; j < app.textFonts.length; j++) {
+  if (app.textFonts[j].typename == font) {
+    textArtRange.characterAttributes.textFont = app.textFonts[j];
+    break;
   }
-
+}
+if(logoMark.width/logoMark.height < 1.5 && logoMark.width/logoMark.height > 0.5){
+  txt.top = hei; //logoCenter[0];
+  txt.left = len; //logoCenter[1];
+  txt.height = logoMark.width/txt.width * txt.height;
+  txt.width = logoMark.width;
+}
+else if(logoMark.width/logoMark.height > 1.5 && logoMark.width/logoMark.height < 0.5 && logoMark.width/logoMark.height < 5 && logoMark.width/logoMark.height > 0.2){
   txt.top = hei; //logoCenter[0];
   txt.left = len; //logoCenter[1];
   txt.height = logoMark.width/txt.width * txt.height;
   txt.width = logoMark.width;
 }
 
+}
+//createImage(myOutput[0], myOutput[1], myOutput[2], myOutput[3], myOutput[4]);
+
 createImage(myOutput[0], myOutput[1], myOutput[2], myOutput[3], myOutput[4]);
+
 //SECTION 3: Analyze the width and height of the image to correctly
 // format it with the text.
 
@@ -375,4 +391,5 @@ textFrame.properties =
 // center variable is the anchor point of the text frame for creation, the we shift the position to center it.
 */
 app.redraw();
+//myOutput;
 //SECTION 4: Export the image out as logo.png
