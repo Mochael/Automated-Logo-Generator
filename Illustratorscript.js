c8853f84ddfbc15519b96c97daad0a9d4c2a337c
Tanishq C Kancharla <tkanchar@bh140d-a2.andrew.cmu.edu>
2018-09-21 22:54:24 -0400

    Changed Illustrator to .js file

---
+++ Illustratorscript.js
@@ -0,0 +1,13 @@
+doc = app.activeDocument;
+//image comes in as image.png
+var image = require("image.png");
+
+
+//SECTION 1: Export image into Illustrator, center it
+
+
+
+//SECTION 2: Live trace the image in black and white, with ignore white set to true
+
+//SECTION 3: Analyze the width and height of the image to correctly
+// format it with the text.
